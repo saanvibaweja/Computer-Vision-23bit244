a2cb9cb9a11e5f18b76066720a3e0cc07d0abdea
Saanvi-Baweja <saanvi.baweja@3spacecorp.com>
2026-08-05 09:20:19 +0530

    Updated exp2.py with median filter implementation

--- exp2.py
+++ exp2.py
@@ -6,7 +6,7 @@ plt.imshow(image,cmap='gray')
   
 cv2.imshow('image',image)
 
-def ass_salt_pepper_noise(image,probability=0.3):
+def add_salt_pepper_noise(image,probability=0.3):
     noisy=image.copy()
     
     random=np.random.rand(*image.shape)
@@ -14,7 +14,33 @@ def ass_salt_pepper_noise(image,probability=0.3):
     noisy[random>1-probability/2]=255
     return noisy
 
-prob=0.2
-noisy_image=ass_salt_pepper_noise(image,prob)
-plt.imshow(noisy_image,cmap='gray')
+# prob=0.2
+# noisy_image=ass_salt_pepper_noise(image,prob)
+# plt.imshow(noisy_image,cmap='gray')
+# plt.show()
+noisy_image = add_salt_pepper_noise(image, probability=0.2)
+
+# Remove noise using Median Filter (Moving Window)
+filtered_image = cv2.medianBlur(noisy_image, 3)
+
+# Display images
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,3,1)
+plt.imshow(image, cmap='gray')
+plt.title("Original Image")
+plt.axis('off')
+
+plt.subplot(1,3,2)
+plt.imshow(noisy_image, cmap='gray')
+plt.title("Salt & Pepper Noise")
+plt.axis('off')
+
+plt.subplot(1,3,3)
+plt.imshow(filtered_image, cmap='gray')
+plt.title("Median Filter Output")
+plt.axis('off')
+
+plt.tight_layout()
 plt.show()
+
